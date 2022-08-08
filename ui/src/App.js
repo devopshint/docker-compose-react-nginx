8c6b1c076e1bf75a5a3d1e5baff626f240058b19
vishnu <simbu.vishwa@gmail.com>
2022-08-08 15:59:37 +0530

    test

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          okks <code>src/App.js</code> and save to reload ok.
+          test <code>src/App.js</code> and save to reload ok.
         </p>
         <a className="App-link" href="http://localhost/api/" target="_blank" rel="noopener noreferrer">
           Learn React
