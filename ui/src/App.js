cfc15b75810db9f9b6bd68f7fa3789256f316fcf
vishnu <simbu.vishwa@gmail.com>
2022-08-18 01:44:45 +0530

    mymy

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">checking custom domainss...</div>;
+  return <div className="App">mymy custom domainssss...</div>;
 }
 export default App;
