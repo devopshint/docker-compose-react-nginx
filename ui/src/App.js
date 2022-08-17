5e52f78ecd2b06ca2a95105579498551c9e6959f
vishnu <simbu.vishwa@gmail.com>
2022-08-18 01:29:36 +0530

    dev prod

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">checking custom domain...</div>;
+  return <div className="App">checking custom domainss...</div>;
 }
 export default App;
